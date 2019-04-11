52f156a8a7ec5b38243874095b6e659b109e10df
azamatikus <dastan_shaukenov@mail.ru>
2019-04-12 01:56:05 +0500

    задание 1 выполнено

--- hw/src/lesson3/task2/Phonebook.java
+++ hw/src/lesson3/task2/Phonebook.java
@@ -8,7 +8,7 @@ public class Phonebook {
     public static void main(String[] args) {
 
         //задание 2 мой вариант
-
+//
         Map<String, Contact> contacts = new HashMap<String, Contact>();
         contacts.put("Петров", new Contact("Петров","87011801842"));
         contacts.put("Иванов", new Contact("Иванов", "87021801843", "87011801842"));
