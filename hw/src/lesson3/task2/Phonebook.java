874aedf18baaef7e2c527efbaf91ba47e98045ac
azamatikus <dastan_shaukenov@mail.ru>
2019-04-12 02:54:53 +0500

    lesson3 corrected

--- hw/src/lesson3/task2/Phonebook.java
+++ hw/src/lesson3/task2/Phonebook.java
@@ -1,25 +1,47 @@
 package lesson3.task2;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Phonebook {
 
+//        //задание 2 мой вариант
+////
+//        Map<String, Contact> contacts = new HashMap<String, Contact>();
+//        contacts.put("Петров", new Contact("Петров","87011801842"));
+//        contacts.put("Иванов", new Contact("Иванов", "87021801843", "87011801842"));
+//        Contact myFriend = contacts.get("Иванов");
+//        System.out.println(myFriend);
+
+    //вывод с ошибкой ((
+
+    //вариант преподавателя
+
+    Map<String, Set<String>> phonebook;
+
+    public Phonebook() {
+        this.phonebook = new HashMap<>();
+    }
+
+    public void addPhone(String name, String phone) {
+//        Set<String> phones = phonebook.computeIfAbsent(name,key -> new HashSet<>());
+        Set<String> phones= phonebook.get(name);
+        if (phones == null){
+            phones = new HashSet<>();
+            phonebook.put(name, phones);
+        }
+        phones.add(phone);
+
+    }
+
+    public Set<String> getPhonesByName(String name) {
+
+        return phonebook.get(name);
+
+    }
+
     public static void main(String[] args) {
-
-        //задание 2 мой вариант
-//
-        Map<String, Contact> contacts = new HashMap<String, Contact>();
-        contacts.put("Петров", new Contact("Петров","87011801842"));
-        contacts.put("Иванов", new Contact("Иванов", "87021801843", "87011801842"));
-        Contact myFriend = contacts.get("Иванов");
-        System.out.println(myFriend);
-
-        //вывод с ошибкой ((
-
-        //вариант преподавателя
-
-
-
+        Phonebook phonebook = new Phonebook();
+        phonebook.addPhone("Маркиз", "123");
     }
-    }
+}
+
