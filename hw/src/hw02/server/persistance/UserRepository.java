46a1e85ba0a78eeea29c008015f9b33993db8fbd
azamatikus <dastan_shaukenov@mail.ru>
2019-05-16 17:46:34 +0500

    hw02 дз

--- hw/src/hw02/server/persistance/UserRepository.java
+++ hw/src/hw02/server/persistance/UserRepository.java
@@ -3,6 +3,7 @@ package hw02.server.persistance;
 import hw02.server.User;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
@@ -12,29 +13,68 @@ public class UserRepository {
     public UserRepository(Connection conn) throws SQLException {
         this.conn = conn;
 
-        // TODO создать таблицу пользователей, если она еще не создана
-    }
+        // создать таблицу пользователей, если она еще не создана
 
+        createTableIfNotExist(conn);
+    }
 
     public void insert(User user) throws SQLException {
 
-        // TODO добавить нового пользователя в БД
+        // добавить нового пользователя в БД
+
+        try(PreparedStatement prep = conn.prepareStatement("insert into users (login, password) values(?, ?);")){
+            prep.setInt(1, -1);
+            prep.setString(2, user.getLogin());
+            prep.setString(3, user.getPassword());
+            prep.execute();
+        }
+
 
     }
 
     public User findByLogin(String login) throws SQLException {
 
-        // TODO найти пользователя в БД по логину
+        // найти пользователя в БД по логину
 
-        return null;
+        try (PreparedStatement stmt = conn.prepareStatement(
+                "select id, login, password from users where login = ?")) {
+            stmt.setString(1, login);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new User(rs.getInt(1), rs.getString(2), rs.getString(3));
+            }
+        }
+        return new User(-1, "", "");
     }
 
     public List<User> getAllUsers() throws SQLException {
 
-        // TODO извлечь из БД полный список пользователей
+        // извлечь из БД полный список пользователей
 
-        return null;
+        List<User> list = new ArrayList<>();
+
+        try (Statement stmt = conn.createStatement()) {
+            ResultSet rs = stmt.executeQuery("select id, login, password from users");
+
+            while (rs.next()) {
+                list.add(new User(rs.getInt(1), rs.getString(2), rs.getString(3)));
+            }
+        }
+        return list;
     }
 
-    // TODO добавить нового пользователя в БД
+    public void createTableIfNotExist(Connection conn) throws SQLException {
+        try(PreparedStatement prep = conn.prepareStatement(
+                "create table if not exists users (\n" +
+                        "\tid int auto_increment primary key,\n" +
+                        "    login varchar(25),\n" +
+                        "    password varchar(25),\n" +
+                        "    unique index uq_login(login)\n" +
+                        ");")){
+
+            prep.execute();
+        }
+
+    }
 }
